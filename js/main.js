3261d14b812144d060fb2039429bd1ea888e694a
Ryan Cooper <ryancooper729@gmail.com>
2021-06-28 00:09:07 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// GLOBALS
+
+// MAP INITIALIZATION
+const map = L.map("map", {
+    center: [35.8, -78.65],
+    zoom: 12
+});
+const basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
