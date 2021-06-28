c5ea8f02e0b5736211cbe9f1d50030e96b9901f7
Ryan Cooper <ryancooper729@gmail.com>
2021-06-28 09:55:56 -0400

    add feature layer and create init function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 // GLOBALS
 
+
 // MAP INITIALIZATION
-const map = L.map("map", {
-    center: [35.8, -78.65],
-    zoom: 12
-});
-const basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
-}).addTo(map);
+function createMap() {
+    const map = L.map("map", {
+        center: [0, 0],
+        zoom: 2
+    });
+    const basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    }).addTo(map);
+
+    // Feature Data
+    getData(map);
+}
+
+function getData(map) {
+    $.ajax("./data/busiest_container_ports_2004_2019.geojson", {
+        dataType: "json",
+        success: function (response) {
+            let featureLayer = L.geoJSON(response);
+            featureLayer.addTo(map);
+            map.fitBounds(featureLayer.getBounds())      
+        }
+    })
+}
+
+$(document).ready(createMap)
